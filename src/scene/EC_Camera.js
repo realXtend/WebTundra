6559496f55080ead8a8df40d78de90ac662d2f04
Erno Kuusela <erno@playsign.net>
2013-12-13 14:17:38 +0200

    Add a few ECs generated with ecgen: EC_Camera, EC_Light, EC_Sound

---
+++ src/scene/EC_Camera.js
@@ -0,0 +1,13 @@
+var cComponentTypeCamera = 15;
+function EC_Camera() {
+    Component.call(this, cComponentTypeCamera);
+    this.addAttribute(cAttributeFloat3, "upVector", "Up vector", {x: 0.0, y: 1.0, z: 1.0});
+    this.addAttribute(cAttributeReal, "nearPlane", "Near plane", 0.1);
+    this.addAttribute(cAttributeReal, "farPlane", "Far plane", 2000.0);
+    this.addAttribute(cAttributeReal, "verticalFov", "Vertical FOV", 45.0);
+    this.addAttribute(cAttributeString, "aspectRatio", "Aspect ratio", "");
+}
+
+EC_Camera.prototype = new Component(cComponentTypeCamera);
+
+registerComponent(cComponentTypeCamera, "Camera", function(){ return new EC_Camera(); });
