7603cf3f1c9212d7401eeccb61efd9ed9cb1ec9b
Tapani J. <tapani.jamsa@gmail.com>
2013-12-16 09:58:36 +0200

    backup commit

--- src/app.js
+++ src/app.js
@@ -9,46 +9,70 @@
 var useSignals = true;
 
 function Application(dataConnection, viewer) {
-    this.keyboard = new THREEx.KeyboardState();
-    this.clock = new THREE.Clock();
-
-    // SCENE
-    this.scene = new THREE.Scene();
-
-    // Camera
-    var SCREEN_WIDTH = window.innerWidth,
-    SCREEN_HEIGHT = window.innerHeight;
-    var VIEW_ANGLE = 45,
-    ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT,
-    NEAR = 0.1,
-    FAR = 20000;
-    this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-    this.scene.add(this.camera);
-    this.camera.position.set(0, 300, 100); // (0, 1000, -375);
-    this.camera.lookAt(this.scene.position);
-
-    // VIEWER
-    this.viewer = new ThreeView(this.scene, this.camera);
+    // CAMERA
+    this.SCREEN_WIDTH = window.innerWidth;
+    this.SCREEN_HEIGHT = window.innerHeight;
+    this.VIEW_ANGLE = 45;
+    this.ASPECT = this.SCREEN_WIDTH / this.SCREEN_HEIGHT;
+    this.NEAR = 0.1;
+    this.FAR = 20000;
 
     // MODEL
     this.dataConnection = new WebTundraModel(this);
-
-    this.dataConnection.meshComponentReady.add(this.viewer.addOrUpdate.bind(this.viewer));
-
-    // CONTROLS
-    this.controls = new THREE.OrbitControls(this.camera, this.viewer.renderer.domElement);
-    this.controls.userZoom = true;
 }
 
 Application.prototype = {
 
     constructor: Application,
 
+    init: function() {
+        this.keyboard = new THREEx.KeyboardState();
+        this.clock = new THREE.Clock();
+        // 
+        // SCENE
+        this.scene = new THREE.Scene();
+
+        // CAMERA
+        this.camera = new THREE.PerspectiveCamera(this.VIEW_ANGLE, this.ASPECT, this.NEAR, this.FAR);
+        this.scene.add(this.camera);
+        this.camera.position.set(0, 300, 100); // (0, 1000, -375);
+        this.camera.lookAt(this.scene.position);
+
+        // VIEWER
+        this.viewer = new ThreeView(this.scene, this.camera);
+
+        // MODEL
+        this.dataConnection.meshComponentReady.add(this.viewer.addOrUpdate.bind(this.viewer));
+
+        // CONTROLS
+        this.controls = new THREE.OrbitControls(this.camera, this.viewer.renderer.domElement);
+        this.controls.userZoom = true;
+    },
+
     start: function() {
-        this.dataConnection.connectClient();
+        this.init();
         this.logicInit();
+        this.dataConnection.connectClient();
         this.frameCount = 0;
         this.update();
+    },
+
+    logicInit: function() {
+        this.cubeCount = 0;
+        var scene = this.dataConnection.scene;
+        this.testEntities = [];
+        console.log("in makeEntities");
+        for (var i = 0; i < this.cubeCount; i++) {
+            var ent = scene.createEntity(i + 1000);
+            this.testEntities.push(ent);
+            var placeable = ent.createComponent("placeable", "Placeable", "");
+            var mesh = ent.createComponent("mesh", "Mesh", "placeable");
+            placeable.transform.value.pos.x = i * 150;
+            placeable.transform.value.pos.y = 150;
+
+            setXyz(placeable.transform.value.scale, 1, 1, 1);
+            mesh.meshRef.ref = "http://kek";
+        }
     },
 
     update: function() {
@@ -109,27 +133,11 @@ Application.prototype = {
             if (placeable !== null)
                 for (j in Object.keys(meshes)) {
                     this.viewer.addOrUpdate(entity, placeable, meshes[j]);
-                }
-        }
-    },
-
-    logicInit: function() {
-        this.cubeCount = 0;
-        var scene = this.dataConnection.scene;
-        this.testEntities = [];
-        console.log("in makeEntities");
-        for (var i = 0; i < this.cubeCount; i++) {
-            var ent = scene.createEntity(i + 1000);
-            this.testEntities.push(ent);
-            var placeable = ent.createComponent("placeable", "Placeable", "");
-            var mesh = ent.createComponent("mesh", "Mesh", "placeable");
-            placeable.transform.value.pos.x = i * 150;
-            placeable.transform.value.pos.y = 150;
-
-            setXyz(placeable.transform.value.scale, 1, 1, 1);
-            mesh.meshRef.ref = "http://kek";
+            }
         }
     }
+
+
 };
 
 var debugOnCheckFail = true;
@@ -140,7 +148,7 @@ function checkDefined() {
             if (debugOnCheckFail) {
                 debugger;
             } else {
-                throw("undefined value, arg #" + i);
+                throw ("undefined value, arg #" + i);
             }
         }
     }
@@ -152,9 +160,9 @@ function check() {
             if (debugOnCheckFail) {
                 debugger;
             } else {
-                throw("untrue value" + arguments[i] + ", arg #" + i);
+                throw ("untrue value" + arguments[i] + ", arg #" + i);
             }
- }
+}
 
 
 function attributeValues(o) {
