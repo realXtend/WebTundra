81f7777486d46da694456e269e9529aeeb10a056
Tapani J. <tapani.jamsa@gmail.com>
2013-12-20 11:56:26 +0200

    Update exampleApp

--- src/exampleApp.js
+++ src/exampleApp.js
@@ -9,6 +9,12 @@
  */
 
 var app = new Application();
-app.dataConnection.host = "localhost";
+
+app.host = "localhost"; // IP to the Tundra server
+app.port = 2345; // and port to the server
 
 app.start();
+
+// app.viewer.useCubes = true; // Use wireframe cube material for all objects
+
+app.connect(app.host, app.port);
