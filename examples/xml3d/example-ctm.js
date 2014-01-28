f4a7bbd9f11fa7cd4f6795d9999cf82367b0fa7f
Erno Kuusela <erno@playsign.net>
2014-01-28 11:08:11 +0200

    Add example-ctm.js that had been left out from git

---
+++ examples/xml3d/example-ctm.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// For conditions of distribution and use, see copyright notice in LICENSE
+/*
+ *      @author Tapani Jamsa
+ *      @author Erno Kuusela
+ *      @author Toni Alatalo
+ *      Date: 2013
+ */
+
+var app = new Application();
+app.start();
+app.viewer.meshReadySig.add(function(meshComp, threeMesh) {
+    // if (/raf22031\.ctm$/.test(meshComp.meshRef.ref) === false)
+    //     return;
+
+    var materialParams = {
+        specular: 0x7f7f7f,
+        color: 0xffffff,
+        map: THREE.ImageUtils.loadTexture("raf22031.jpg")
+    };
+    var rafCarMaterial = new THREE.MeshPhongMaterial(materialParams);
+    threeMesh.material = rafCarMaterial;
+    console.log("swapped custom material");
+});
+
+loadXml3d(app.dataConnection, "xml3d-scene-ctm.html");
+
+function loadXml3d(model, docurl) {
+    var parser = new SceneParser(model);
+    parser.parseFromUrlXml3D(docurl);
+    console.log("parse done");
+}
+
