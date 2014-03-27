5efa801b2efe50772fbc4595bea4f195d6e75001
Toni Alatalo <toni@playsign.net>
2014-03-27 15:20:12 +0200

    embed xml3d: after embeds, the js in all examples became identical. is now a little util func in loadxml3d.js that all xml3d loading examples, except the more customized ctm one, reuse.

--- examples/xml3d/loadxml3d.js
+++ examples/xml3d/loadxml3d.js
@@ -5,17 +5,19 @@
  *      @author Tapani Jamsa
  *      @author Erno Kuusela
  *      @author Toni Alatalo
- *      Date: 2013
+ *      Date: 2014
  */
-
-var app = new Application();
-app.start();
-var parser = new SceneParser(app.dataConnection);
-parser.parseDocXml3D(document);
 
 function setupEditorControls() {
     var controls = new THREE.EditorControls(app.viewer.camera, app.viewer.renderer.domElement);
 }
 
-// hack to get the right xml3d-created camera and not the default one.
-window.setTimeout(setupEditorControls, 2000);
+function loadxml3d() {
+    var app = new Application();
+    app.start();
+    var parser = new SceneParser(app.dataConnection);
+    parser.parseDocXml3D(document);
+
+    // hack to get the right xml3d-created camera and not the default one.
+    window.setTimeout(setupEditorControls, 2000);
+}
