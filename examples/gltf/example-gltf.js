6f30cfe3a6b1f7f15be9a50b8d0c8a869a30173e
Toni Alatalo <toni@playsign.net>
2014-03-27 14:41:01 +0200

    embedded xml3d: port gltf duck example too, remove the now extra separate xml3d file

--- examples/gltf/example-gltf.js
+++ examples/gltf/example-gltf.js
@@ -11,16 +11,12 @@
 var app = new Application();
 app.start();
 
+var parser = new SceneParser(app.dataConnection);
+parser.parseDocXml3D(document);
+
 function setupEditorControls() {
     var controls = new THREE.EditorControls(app.viewer.camera, app.viewer.renderer.domElement);
 }
 
-function loadXml3d(model, docurl) {
-    var parser = new SceneParser(model);
-    parser.parseFromUrlXml3D(docurl);
-    console.log("parse done");
-}
-
-loadXml3d(app.dataConnection, "xml3d-scene-gltf.html");
 // hack to get the right xml3d-created camera and not the default one.
 window.setTimeout(setupEditorControls, 2000);
