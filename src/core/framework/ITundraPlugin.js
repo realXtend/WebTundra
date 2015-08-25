d051119df1548a07e163adc6935c0280f3caf384
Cvetan Stefanovski <cvetan5@gmail.com>
2015-08-25 17:36:07 +0300

    ITundraPlugin docs update

--- src/core/framework/ITundraPlugin.js
+++ src/core/framework/ITundraPlugin.js
@@ -4,40 +4,46 @@ define([
         "core/framework/TundraLogging"
     ], function(Class, TundraLogging) {
 
-/**
-    Tundra plugin interface.
-
-    @class ITundraPlugin
-    @constructor
-    @param {String} name Name of the plugin.
-*/
 var ITundraPlugin = Class.$extend(
+/** @lends ITundraPlugin.prototype */
 {
-    __init__ : function(name)
+    /**
+        Tundra plugin interface that implementations need to extend.
+
+        @constructs
+        @param {String} name Name of the plugin.
+        @param {String|Array.<String>} aliases Name aliases.
+    */
+    __init__ : function(name, aliases)
     {
-        if (typeof name !== "string")
-            console.error("ITundraPlugin constructor must be called with the plugin name as a string!");
+        if (typeof name !== "string" || name === "")
+            console.error("ITundraPlugin constructor must be called with name that is a non-empty string!");
 
         /**
-            TundraSDK Framework object.
-            @property framework
-            @type TundraSDK.framework
+            Tundra framework object.
+            @var {Object}
         */
         this.framework = null;
         /**
             Name of the plugin.
-            @property framework
-            @type String
+            @var {String}
         */
         this.name = name;
         /**
+            Name aliases of the plugin.
+            @var {Array.<String>}
+        */
+        this.nameAliases = (typeof aliases === "string" ? [ aliases ] : (Array.isArray(aliases) ? [].concat(aliases) : []));
+        /**
+            If plugin is loaded.
+            @var {Boolean}
+        */
+        this.loaded = false;
+        /**
             Logger for this plugin.
-            @property log
-            @type TundraLogger
+            @var {TundraLogger}
         */
         this.log = TundraLogging.getLogger(this.name);
-
-        this.initialized = false;
     },
 
     _setFramework : function(fw)
@@ -46,25 +52,36 @@ var ITundraPlugin = Class.$extend(
     },
 
     /// Internal
-    _initialize : function()
+    _initialize : function(options)
     {
-        if (this.initialized === true)
+        if (this.loaded === true)
         {
-            this.log("Already initialized!");
+            this.log("Already loaded!");
             return;
         }
 
-        this.initialize();
-        this.initialized = true;
+        this.initialize(options);
+        this.loaded = true;
+    },
+
+    /**
+        Called to resolve property name for Tundra.plugins.<name> registration.
+        Implementations can override this, if not overridden will return plugin name.
+
+        The returned name will be lowercased by Tundra, eg. MyPlugin > myPlugin, Voip > voip.
+
+        @return {String} The name of the plugin property
+    */
+    pluginPropertyName : function()
+    {
+        return this.name;
     },
 
     /**
         Called when plugin is loaded to TundraClient. Implementation can override this.
             <b>Note:</b> this.framework is set at this stage.
-
-        @method initialize
     */
-    initialize : function()
+    initialize : function(options)
     {
     },
 
@@ -78,8 +95,6 @@ var ITundraPlugin = Class.$extend(
         Called after all plugins have been loaded to TundraClient.
         Useful if your plugin needs to fetch and use other plugins.
         Implementation can override this.
-
-        @method postInitialize
     */
     postInitialize : function()
     {
@@ -90,14 +105,12 @@ var ITundraPlugin = Class.$extend(
     {
         this.uninitialize();
         this.framework = null;
-        this.initialized = false;
+        this.loaded = false;
     },
 
     /**
         Called when plugin is unloaded from TundraClient. Implementation can override this.
             <b>Note:</b> this.framework is still valid at this stage.
-
-        @method uninitialize
     */
     uninitialize : function()
     {
