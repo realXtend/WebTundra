1d5e4c9eed90b254aaed58a98ad6611a33c2903f
mabdrabo <m.latief@gmail.com>
2015-12-29 20:51:27 +0200

    feat(): Implement EC_Sound_WebAudio and clean AudioAPI
    EC_Sound_WebAudio
    - Load sound using HTMLAudioElement for enhanced buffering and playback control
    - Create Audio() object, and use it to create Web Audio API MediaElementSourceNode
    - Update PannerNode position to Placeable component and listen to changes
    - Reflect changes to Gain, Loop and Spatial attributes
    
    AudioAPI
    - Remove test sound sources
    - Remove unnecessary pannerNode
    - Assume again one SoundListener
    - Remove loading sound as Asset
    
    Examples
    - Add sample scene that uses AudioAPI, and EC_Sound component
    - Add sample viper.ogg file from MDN Web Audio API samples

--- src/core/audio/AudioAPI.js
+++ src/core/audio/AudioAPI.js
@@ -52,17 +52,10 @@ var AudioAPI = ITundraAPI.$extend(
     {
         this.context = new (window.AudioContext || window.webkitAudioContext)();
 
-        this.panner = this.context.createPanner();
-        //@TODO Remove oscillator if not needed? Might be useful for some custom sound effects.
-        this.oscillator = this.context.createOscillator();
-        this.masterGainNode = this.context.createGain();
-
         // Master GainNode to be used as destination of other sources
-        this.masterGainNode.connect(this.context.destination);
+        this.masterGainNode = this.context.createGain();
         this.masterGainNode.gain.value = this.options.masterGain;
-
-        this.oscillator.connect(this.masterGainNode);
-        this.panner.connect(this.masterGainNode);
+        this.masterGainNode.connect(this.context.destination);
 
         // Register stuff here.
     	this.registerAssetFactories();
@@ -72,76 +65,8 @@ var AudioAPI = ITundraAPI.$extend(
             this.registerEvents();
         else
             Tundra.client.onConnected(this, this.registerEvents);
-    },
 
-    /*
-     * Loads a sound based on the ref-param (should be a website)
-     * The source should be the EC_Sound-component's source. Pass it in.
-     * Loop should be true if you want to loop the sound forever.
-     * The return function should be allowed to be given a source param to.
-     */
-    loadSound : function(ref, source, loop, returnFunction)
-    {
-        if (Tundra.audio.context.listener == null || !Tundra.audio.sceneHasActiveSoundListener())
-            console.warn("[AudioAPI] No active sound listeners.");
-
-        Tundra.asset.requestAsset(ref).onCompleted(this, function(asset) {
-            Tundra.audio.context.decodeAudioData(asset.data, function(decodedData) {
-                // Create the source by using context, set the buffer
-                source.buffer = decodedData;
-
-                // Set loop settings. loopEnd & loopStart default are zeros. loop is a bool value.
-                source.loopEnd = decodedData.duration;
-                source.loop = loop;
-
-                // On ended? Maybe inform the user in some situations? Or something like that.
-                source.onended = function() { };
-
-                // Playback rate, default is 1, 2 is twice the normal speed?
-                source.playbackRate.value = 1;
-
-                source.connect(Tundra.audio.panner);
-
-                returnFunction(source);
-            }.bind(this));
-        }.bind(this));
-    },
-
-    /*
-     * @TODO There can currently, in a really specific situation, be 2 active sound listeners.
-     * Please make it so that there can only be one.
-     */
-    setActiveSoundListener : function(soundListener)
-    {
-        var soundListenerEntities = Tundra.scene.entitiesWithComponent("SoundListener");
-        for (var i = 0; i < soundListenerEntities.length; i++)
-        {
-            var current = soundListenerEntities[i].soundListener;
-
-            if (current.id !== soundListener.id && current.active)
-            {
-                current.setAttribute("active", false, 0);
-                break;
-            }
-        }
-    },
-
-    sceneHasActiveSoundListener : function()
-    {
-        var soundListenerEntities = Tundra.scene.entitiesWithComponent("SoundListener");
-
-        if (soundListenerEntities.length <= 0)
-            return false;
-
-        for (var i = 0; i < soundListenerEntities.length; i++)
-        {
-            var current = soundListenerEntities[i].soundListener;
-
-            if (current.active)
-                return true;
-        }
-
-        return false;
+        console.log('AudioAPI initialized!');
     },
 
     /// ITundraAPI override
