17bf7c3de4abb15be2d19a1d94a6d1846b89165e
mabdrabo <m.latief@gmail.com>
2015-12-29 20:51:27 +0200

    feat(): Implement EC_Sound_WebAudio and clean AudioAPI
    EC_Sound_WebAudio
    - Load sound using HTMLAudioElement for enhanced buffering and playback control
    - Create Audio() object, and use it to create Web Audio API MediaElementSourceNode
    - Update PannerNode position to Placeable component and listen to changes
    - Reflect changes to Gain, Loop and Spatial attributes
    
    AudioAPI
    - Remove test sound sources
    - Remove unnecessary pannerNode
    - Assume again one SoundListener
    - Remove loading sound as Asset
    
    Examples
    - Add sample scene that uses AudioAPI, and EC_Sound component
    - Add sample viper.ogg file from MDN Web Audio API samples

--- src/core/audio/entity-components/EC_Sound_WebAudio.js
+++ src/core/audio/entity-components/EC_Sound_WebAudio.js
@@ -4,10 +4,7 @@ define([
     ], function(EC_Sound) {
 
 /**
-    This base implementation does not do anything. It declared the static attribute structure of EC_Sound
-    in the Tundra protocol.
-
-    Renderer implementations need to provide this components functionality, preferably by extending this object.
+    This is implementation of EC_Sound using Web Audio API.
 
     @class EC_Sound_WebAudio
     @extends EC_Sound
@@ -25,51 +22,229 @@ var EC_Sound_WebAudio = EC_Sound.$extend(
         Implementation : "webaudio"
     },
 
-    /*
-     * The initialization function
-     */
+    /**
+        Called only ONCE when the component is initialized
+    */
     update : function()
     {
-        this.source = Tundra.audio.context.createBufferSource();
+        this.cache = {
+            //_soundGainNode: Tundra.audio.context.createGain(),
+            _position: new THREE.Vector3(),
+            _pannerNode: Tundra.audio.context.createPanner(),
+            _placeable: undefined
+        };
 
-        // @todo Remove, used for test only
-        this.load();
+        this.cache._pannerNode.setPosition(this.cache._position.x, this.cache._position.y, this.cache._position.z);
+        //this.cache._soundGainNode.gain.value = this.soundGain;
+
+        this.cache._pannerNode.connect(Tundra.audio.masterGainNode);
+        //this.cache._soundGainNode.connect(Tundra.audio.masterGainNode);
+        
+        this.checkAndUpdatePlaceable();
+        this.checkAndUpdateSoundRef();
+
+        // Register events listener for creating/removing placeable components 
+        this.parentEntity.onComponentCreated(this, this.onComponentCreated);
+        this.parentEntity.onComponentRemoved(this, this.onComponentRemoved);
+
+        console.log('EC_Sound_WebAudio initialized!');
+    },
+    
+    startPlaying: function()
+    {
+        if(this.cache._element)
+        {
+            this.cache._element.start();
+        }
+    },
+
+    stopPlaying: function()
+    {
+        if(this.cache._element)
+        {
+            this.cache._element.stop();
+        }
+    },
+
+    pausePlaying: function()
+    {
+        if(this.cache._element)
+        {
+            this.cache._element.pause();
+        }
+    },
+
+    _testGetAudioElement: function (soundRef)
+    {
+        var audioElement = new Audio(soundRef);
+        return audioElement;
+    },
+
+    checkAndUpdateSoundRef: function()
+    {
+        if(this.soundRef)
+        {
+            var _element = this._testGetAudioElement(this.soundRef);
+            //Tundra.asset.requestAsset(newSoundRef).onCompleted(this, function (asset)
+            //{
+                // TODO: Properly dispose old HTMLAudioElement and MediaElementSourceNode
+                // for now just disconnect the old source
+                if(this.cache._element)
+                    this.cache._element.stop();
+
+                if(this.cache._source)
+                    this.cache._source.disconnect();
+
+                this.cache._element = _element;
+                this.cache._source =  Tundra.audio.context.createMediaElementSource(_element);
+
+                if(this.playOnLoad)
+                {
+                    // Setting it to false still performs autoplay, 
+                    // so set it only if playOnLoad
+                    _element.autoplay = true;
+                }
+
+                _element.loop = this.loopSound;
+                _element.volume = this.soundGain;
+
+                this._reconnectSourceNode();
+
+            //}.bind(this));
+        }
+        // TODO: What to do if set to empty string
+    },
+
+    _reconnectSourceNode: function()
+    {
+        if(this.cache._source)
+        {
+            console.log('### EC_Sound_WebAudio, _reconnectSourceNode - spatial: ', this.spatial, ', pannerNode:', this.cache._pannerNode, ', placeable:', this.cache._placeable);
+            this.cache._source.disconnect();
+            // Spatial is not set, connect to gain node regardless of placeable or pannerNode
+            if(!this.spatial)
+            {
+                this.cache._source.connect(Tundra.audio.masterGainNode);
+                console.log('### EC_Sound_WebAudio, _reconnectSourceNode - connect to masterGainNode');
+            }
+            // Spatial is set, pannerNode exists, placeable is available
+            else if(this.spatial && this.cache._pannerNode && this.cache._placeable)
+            {
+                // It is safe to call disconnect() even if not connected before
+                this.cache._source.connect(this.cache._pannerNode);
+                console.log('### EC_Sound_WebAudio, _reconnectSourceNode - connect to pannerNode');
+            }
+            // Something is wrong (spatial is set but no pannerNode or no placeable), stop playing
+            else
+            {
+                if(this.cache._element)
+                    this.cache._element.stop();
+            }
+        }
+
+    },
+
+    _updatePosition: function()
+    {
+        if(this.cache._pannerNode && this.cache._placeable)
+        {
+            this.parentEntity.placeable.worldPosition(this.cache._position);
+            this.cache._pannerNode.setPosition(this.cache._position.x, this.cache._position.y, this.cache._position.z);
+        }
+    },
+
+    checkAndUpdatePlaceable: function()
+    {
+        /* If there is already a placeable attached do nothing (only first placeable component is considered) */
+        if(this.cache._placeable)
+            return;
+
+        /* Otherwise, get read its initial position and attach an attribute change listener to it */
+        if(this.hasParentEntity() && this.parentEntity.placeable)
+        {
+            console.log('### EC_Sound_WebAudio, set placeable:', this.parentEntity.placeable);
+            this.cache._placeable = this.parentEntity.placeable;
+            
+            this._updatePosition();
+
+            // TODO: Check if unsubscribe is required later
+            this.parentEntity.placeable.onAttributeChanged(this, function (entity, component, attributeIndex, attributeName, attributeValue){
+                /* Check if transform attribute (attribute index = 0) changed */
+                if(attributeIndex==0)
+                {
+                    this._updatePosition();
+                }
+            }.bind(this));
+
+        }
+    },
+
+    onComponentCreated: function(ent, component)
+    {
+        /* Check if the created component is Placeable */
+        if(component.id==20)
+        {
+            this.checkAndUpdatePlaceable();
+            this._reconnectSourceNode();
+        }
+    },
+
+    onComponentRemoved: function(ent, component)
+    {
+        /* Check if the removed component is Placeable and the same placeable cached */
+        if(component.id==20 && this.cache._placeable && this.cache._placeable.id === component.id)
+        {
+            this.cache._placeable = undefined;
+            this._reconnectSourceNode();
+        }
+    },
+
+    changeGain: function()
+    {
+        if(this.cache._element)
+        {
+            this.cache._element.volume = this.soundGain;
+        }
+    },
+
+    changeLoopSound: function()
+    {
+        if(this.cache._element)
+        {
+            this.cache._element.loop = this.loopSound;
+        }
+
     },
 
     attributeChanged : function(index, name, value)
     {
         switch(index)
         {
-        case 0: // sound ref
-            this.load();
+        case 0: // soundRef
+            this.checkAndUpdateSoundRef();
             break;
-        case 4: // play on creation
+        case 3: // soundGain
+            this.changeGain();
             break;
-        case 5: // loop sound
-            if (this.source != null)
-                this.source.loop = value;
+        case 4: // playOnLoad
+            break;
+        case 5: // loopSound
+            this.changeLoopSound();
+            break;
+        case 6: // spatial
+            this._reconnectSourceNode();
             break;
         default:
             break;
         }
     },
 
-    load : function()
+    load : function(value)
     {
-        if (this.soundRef != "" && (this.soundRef.indexOf(".mp3") > -1 || this.soundRef.indexOf(".ogg") > -1))
-            Tundra.audio.loadSound(this.soundRef, this.source, this.loopSound, this.soundLoad.bind(this));
+
     },
 
-    soundLoad : function(newSource) 
-    {
-        this.source = newSource;
 
-        if (this.source != null)
-        {
-            if (this.playOnLoad)
-                this.source.start();
-        }
-    }
 });
 
 return EC_Sound_WebAudio;
