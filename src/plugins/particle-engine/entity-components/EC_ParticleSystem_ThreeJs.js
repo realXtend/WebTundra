cad8b6d7afaa10337920ee76bd7ee1d689257d64
Cvetan Stefanovski <cvetan5@gmail.com>
2016-09-15 13:31:17 +0300

    ParticleSystem: Null check

--- src/plugins/particle-engine/entity-components/EC_ParticleSystem_ThreeJs.js
+++ src/plugins/particle-engine/entity-components/EC_ParticleSystem_ThreeJs.js
@@ -81,6 +81,9 @@ var EC_ParticleSystem_ThreeJs = EC_ParticleSystem.$extend(
     _onParticleLoaded: function(asset)
     {
         this.system = this.engine.createSystem(this.systemId, asset.data);
+        if (!this.system)
+            return;
+
         this.properties = this.system.exportProperties();
         this.checkAndUpdatePlaceable();
     },
